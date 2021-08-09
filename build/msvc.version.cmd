7decd4e20d5ef77d972e1fd2a6eebaad212e9de5
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 20:25:41 +0300

    Update

---
+++ build/msvc.version.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> version quantum-script-extension-md5
+
+xyo-cc --mode=version quantum-script-extension-md5
