1010e62e5d7c019eddfb17f1abddcef05bbd3a3b
Grigore Stefan <g_stefan@yahoo.com>
2021-08-09 15:43:11 +0300

    Update

---
+++ build/msvc.version-dependency-update.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+echo -^> version-dependency-update quantum-script-extension-md5
+
+xyo-cc --update-version-dependency quantum-script-extension-md5
