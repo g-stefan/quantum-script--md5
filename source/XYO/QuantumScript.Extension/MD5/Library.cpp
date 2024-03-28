bb64a35ad41add3f15f6dd3c1c449681cc4fb305
Grigore Stefan <g_stefan@yahoo.com>
2024-03-28 12:11:13 +0200

    Update

--- source/XYO/QuantumScript.Extension/MD5/Library.cpp
+++ source/XYO/QuantumScript.Extension/MD5/Library.cpp
@@ -54,7 +54,7 @@ namespace XYO::QuantumScript::Extension::MD5 {
 
 };
 
-#ifdef XYO_COMPILE_DYNAMIC_LIBRARY
+#ifdef XYO_PLATFORM_COMPILE_DYNAMIC_LIBRARY
 extern "C" XYO_QUANTUMSCRIPT_EXTENSION_MD5_EXPORT void quantumScriptExtension(XYO::QuantumScript::Executive *executive, void *extensionId) {
 	XYO::QuantumScript::Extension::MD5::initExecutive(executive, extensionId);
 };
